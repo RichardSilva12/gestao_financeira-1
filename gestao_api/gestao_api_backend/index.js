e76ae1e333c3c159c52c0bf334c506cba1d75455
Matheus <matheusleonardi07gmail.com>
2025-05-22 22:29:04 -0300

    teste

--- gestao_api/gestao_api_backend/index.js
+++ gestao_api/gestao_api_backend/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");  //teste 01
 const sqlite3 = require("sqlite3").verbose();
 
-const app = express();
+const app = express(); 
 app.use(cors());
 app.use(bodyParser.json());
 
